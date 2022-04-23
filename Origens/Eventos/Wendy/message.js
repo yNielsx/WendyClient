0ceeecc96dd89fa8e0263f1a2cc12a36490fe59b
yNielsx <103971179+yNielsx@users.noreply.github.com>
2022-04-23 13:29:21 -0300

    Update message.js

--- Origens/Eventos/Wendy/message.js
+++ Origens/Eventos/Wendy/message.js
@@ -4,7 +4,7 @@ module.exports = class {
   constructor(client) {
     this.client = client;
     this.name = 'messageCreate';
-    this.prefix = '!!';
+    this.prefix = 'SEU PREFIXO';
   }
   async executar(message) {
    if (message.author.bot || !message.content.startsWith(this.prefix)) return;
@@ -16,8 +16,8 @@ module.exports = class {
         if (!cmd) return;
         if (cmd.cooldown.has(message.author.id)) return message.channel.send({ embeds: [
           new MessageEmbed()
-          .setTitle('oi')
-          .setDescription('sh')
+          .setTitle('** ERROR 404**')
+          .setDescription(`Você precisa esperar ${Math.floor((cmd.cooldown/1000) % 60)} segundos para utilizar esse comando novamente.`)
         ]}) && message.delete();
 
         cmd.setMessages(message);
